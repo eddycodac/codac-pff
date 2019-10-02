e70b676750ea705379b6dd53013a2443c292b4fc
LeTib0 <LeTib0@users.noreply.github.com>
2019-10-02 17:27:41 +0400

    todo ttest

--- meteor/server/main.js
+++ meteor/server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import Links from '/imports/api/links';
+import '../imports/api/tasks.js';
 
 function insertLink(title, url) {
   Links.insert({ title, url, createdAt: new Date() });
