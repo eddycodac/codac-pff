d54d839c5d06fa9588de56eecbdbf18a1174430f
LeTib0 <LeTib0@users.noreply.github.com>
2019-10-09 15:12:48 +0400

    Mise en place de form v0.1

--- client/main.js
+++ client/main.js
@@ -4,6 +4,11 @@ import { FlowRouter } from 'meteor/ostrio:flow-router-extra'
 import './startup/router'
 import './ui/layout/layout'
 
+if (Meteor.isDevelopment){
+    window.Messages = Messages
+}
+
 if (MediaStreamError.isDevelopment){
     window.FlowRouter = FlowRouter
 }
+
--- server/main.js
+++ server/main.js
@@ -1,6 +1,3 @@
-import '../both'
 import { Meteor } from 'meteor/meteor';
+import '../both'
 
-Meteor.startup(() => {
-  // code to run on server at startup
-});
