b85a34a2b885d24aace631fb878237fe95adb7e0
LeTib0 <LeTib0@users.noreply.github.com>
2019-10-10 20:55:08 +0400

    mise en place du  modal sur l'accueil

--- client/ui/home/home.js
+++ client/ui/home/home.js
@@ -1,3 +1,26 @@
 import './home.html'
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra'
 import './../book_sam/book_sam'
 import './../I_Am_Sam/IAmSam'
+
+Template.home.events({
+    'click .js-open-log-modal'(event, instance){
+        Modal.show('logModal')
+    },
+    'click .js-logout'(event, instance){
+        Meteor.logout()
+        FlowRouter.go('/')
+    }
+})
+
+Template.logModal.onCreated(function() {
+    this.autorun(() =>{
+        if(Meteor.userId()){
+            Modal.hide('logModal')
+            if (Session.get('redirection')) {
+                FlowRouter.go(Session.get('redirection'))
+                Session.set('redirection', undefined)
+            }
+        }
+    })
+})
