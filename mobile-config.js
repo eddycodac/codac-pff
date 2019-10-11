d5c3ed972140cc98c49b9ca7471e91476159a50c
eddyrlf <eddy.roulof@coding-academy.fr>
2019-10-11 14:29:50 +0400

    ajout mobileconfig pour ajout logo

---
+++ mobile-config.js
@@ -0,0 +1,15 @@
+App.icons({
+    'android_mdpi': 'icons/icon-60@2x.png',
+    'android_hdpi': 'icons/icon-60@2x.png',
+    'android_xhdpi': 'icons/icon-60@2x.png',
+    'android_xxhdpi': 'icons/icon-60@2x.png',
+    'android_xxxhdpi': 'icons/icon-60@2x.png',
+    // 'iphone_3x': 'icons/icon-60@3x.png',
+    // More screen sizes and platforms...
+  });
+  
+  // App.launchScreens({
+  //   'iphone_2x': 'splash/Default@2x~iphone.png',
+  //   'iphone5': 'splash/Default~iphone5.png',
+  //   // More screen sizes and platforms...
+  // });
